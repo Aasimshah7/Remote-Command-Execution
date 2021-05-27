ece08b92482bd8d0ac2da825d2726e545b830aec
Aasimshah7 <59438214+Aasimshah7@users.noreply.github.com>
2021-05-27 07:47:44 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,36 @@
+import os
+import socket
+
+
+def exec_cmd_2(cmd):
+    myCmd = os.popen(cmd).read()
+    # print(myCmd)
+    return myCmd
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+if _name == 'main_':
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(('', 12000))
+    while True:
+        message, address = server_socket.recvfrom(1024)
+
+        print('Client wants to execute : ' + str(message, 'utf-8'))
+
+        cmd = message.decode("utf-8")
+        print("Returning output: ")
+
+        myMsg = exec_cmd_2(cmd)
+        print(myMsg)
+        msg = bytes(myMsg, 'utf-8')
+        server_socket.sendto(msg, address)
